97cc1485d491bad2d1e7b7feb93c48763dd2f0ba
Yana Tokareva <yanatokareva01@gmail.com>
2018-04-27 17:45:45 +0300

    add PUT /books/:id

--- controllers/books.js
+++ controllers/books.js
@@ -53,5 +53,39 @@ module.exports.add = async (ctx, next) => {
 };
 
 module.exports.update = async (ctx, next) => {
+	const bookId = ctx.params.id;
+	let book = null;
 
+	try {
+		book = {
+			title: ctx.request.body.title || null,
+			date: ctx.request.body.date ? (new Date(ctx.request.body.date)).toISOString().substring(0, 10) : null,
+			author: ctx.request.body.author || null,
+			description: ctx.request.body.description || null,
+			image: ctx.request.body.image || null
+		};
+	} catch (err) {
+		ctx.status = 400;
+		return;
+	}
+
+	try {
+		let request = `UPDATE books SET `;
+		if (book.title || book.date || book.author || book.description || book.image) {
+			request += `${book.title ? `title='${book.title}',`: ''}`;
+			request += `${book.date ? `date='${new Date(book.date).toISOString().substring(0, 10)}',`: ''}`;
+			request += `${book.author ? `author='${book.author}',`: ''}`;
+			request += `${book.description ? `description='${book.description}',`: ''}`;
+
+			request = request.substring(0, request.length - 1) +  ` WHERE id = ${bookId};`;
+
+			const result = await db.query(request);
+		}
+
+		ctx.body = "ok";
+
+	} catch (err) {
+		ctx.status = 500;
+		ctx.body = err;
+	}
 };
